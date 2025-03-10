5b0e784cc12ee9508085f6524ea6cb896b87e75f
CellarBit <marc_reguant@outlook.com>
2025-03-10 22:32:30 +0800

    j

--- Magic Ruler/js/OrbitControls.js
+++ Magic Ruler/js/OrbitControls.js
@@ -41,8 +41,8 @@
 
 			// How far you can orbit vertically, upper and lower limits.
 			// Range is 0 to Math.PI radians.
-			this.minPolarAngle = - Infinity; // radians
-			this.maxPolarAngle = Infinity; // radians
+			this.minPolarAngle = 0; // radians
+			this.maxPolarAngle = Math.PI; // radians
 
 			// How far you can orbit horizontally, upper and lower limits.
 			// If set, the interval [ min, max ] must be a sub-interval of [ - 2 PI, 2 PI ], with ( max - min < 2 PI )
